cd4a271133bd13359cb760d7d8b8ebf56cba96c4
Tsegenet-hub <55013639+Tsegenet-hub@users.noreply.github.com>
2020-01-02 12:17:39 -0500

    final projest

---
+++ data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+  {
+    name: "Hanna",
+    photo: "./images/Hanna.jpg",
+    scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+  },
+  {
+    name: "Elena",
+    photo: "./images/elena.jpg",
+    scores: [1, 1, 4, 2, 2, 3, 2, 5, 4, 1]
+  },
+  {
+    name: "Robel",
+    photo: "./images/robel.jpg",
+    scores: [2, 3, 1, 4, 1, 3, 1, 5, 1, 1]
+  },
+  {
+    name: "Liya",
+    photo: "./images/liya.jpg",
+    scores: [3, 4, 5, 2, 3, 5, 5, 4, 4, 2]
+  },
+  {
+    name: "Helen",
+    photo: "./images/helen.jpg",
+    scores: [3, 2, 3, 1, 1, 3, 1, 2, 2, 1]
+  }
+];
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+var path = require("path");
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("Listening on PORT:" + PORT);
+});
